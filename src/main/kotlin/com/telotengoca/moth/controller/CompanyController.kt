2c63a43589d3375a25748136706546e2566b10dc
Brian Caldera <brian_alex07@outlook.com>
2023-04-14 18:27:11 -0400

    company view and controller.

---
+++ src/main/kotlin/com/telotengoca/moth/controller/CompanyController.kt
@@ -0,0 +1,11 @@
+package com.telotengoca.moth.controller
+
+import com.telotengoca.moth.logger.MothLoggerFactory
+
+class CompanyController {
+
+    companion object {
+        private val logger = MothLoggerFactory.getLogger(CompanyController::class.java)
+    }
+
+}
