f68e22bcc1e7be3bf2cabbf1eb4dff09d4b92e21
Brian Caldera <brian_alex07@outlook.com>
2023-04-14 03:40:16 -0400

    deleteCompany function defined.

--- src/test/kotlin/com/telotengoca/moth/model/CompanyManager.kt
+++ src/test/kotlin/com/telotengoca/moth/model/CompanyManager.kt
@@ -15,17 +15,19 @@ data class Company(
 
 interface CompanyManager {
     fun add(company: Company): Unit
-    fun getCompanies(): Unit
-    fun deleteCompanies(): Unit
+    fun getCompanies(): List<Company>
+    fun deleteCompany(rif: String): Unit
 }
 
 class CompanyManagerImpl(private val database: MothDatabase) : CompanyManager {
     companion object {
         private val logger: MothLogger = MothLoggerFactory.getLogger(CompanyManager::class.java)
         private const val TABLE_NAME: String = "company"
+        const val RESOURCE: String = "company"
     }
 
     class CompanyRIFDuplicated(message: String) : RuntimeException(message)
+    class CompanyNotFound(message: String) : RuntimeException(message)
 
     init {
         createCompanyTable()
@@ -37,12 +39,30 @@ class CompanyManagerImpl(private val database: MothDatabase) : CompanyManager {
         }
     }
 
-    override fun getCompanies() {
+    override fun getCompanies(): List<Company> {
         TODO("Not yet implemented")
     }
 
-    override fun deleteCompanies() {
-        TODO("Not yet implemented")
+    override fun deleteCompany(rif: String) {
+        database.connectDatabase().use { con ->
+            con.prepareStatement("SELECT COUNT(*) FROM `$TABLE_NAME` WHERE `rif` = ?").use { stm ->
+                stm.setString(1, rif)
+                stm.executeQuery().use {rs->
+                    val check = rs.next()
+                    check(check)
+                    val count = rs.getInt(1)
+                    if (count == 0) throw CompanyNotFound("The company that you are trying to delete does not exist")
+                    check(count < 2)
+                }
+            }
+
+            con.prepareStatement("DELETE FROM `$TABLE_NAME` WHERE `rif` = ?").use {stm ->
+                stm.setString(1, rif)
+                val result = stm.executeUpdate()
+                check(result == 1)
+                logger.info("Company with rif [{}] has been deleted", rif)
+            }
+        }
     }
 
     private fun createCompanyTable() {
@@ -57,7 +77,7 @@ class CompanyManagerImpl(private val database: MothDatabase) : CompanyManager {
             logger.info("Creating table '{}'...", TABLE_NAME)
 
             con.createStatement().use {stm ->
-                stm.execute("CREATE TABLE IF NOT EXISTS `company`(`rif` VARCHAR(10)  PRIMARY KEY NOT NULL, `name` VARCHAR(50) NOT NULL, `address` VARCHAR(100), `telephone` VARCHAR(15), `telephone_2` VARCHAR(15), `email` VARCHAR(254), `alias` VARCHAR(50) NOT NULL)")
+                stm.execute("CREATE TABLE IF NOT EXISTS `$TABLE_NAME`(`rif` VARCHAR(10)  PRIMARY KEY NOT NULL, `name` VARCHAR(50) NOT NULL, `address` VARCHAR(100), `telephone` VARCHAR(15), `telephone_2` VARCHAR(15), `email` VARCHAR(254), `alias` VARCHAR(50) NOT NULL)")
             }
 
             logger.info("Table '{}' created", TABLE_NAME)
