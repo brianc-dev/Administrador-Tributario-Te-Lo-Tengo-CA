5f9b137fcbd1cc9f17e7fad9f0eca7ef94908b70
Brian Caldera <brian_alex07@outlook.com>
2023-04-14 14:42:18 -0400

    added another test

--- src/test/kotlin/com/telotengoca/moth/model/CompanyManager.kt
+++ src/test/kotlin/com/telotengoca/moth/model/CompanyManager.kt
@@ -211,4 +211,42 @@ class CompanyManageImplTest() {
         }
 
     }
+
+    @Test
+    fun `test that information is in order`() {
+        // given
+        val rif = "J123456789"
+        val name = "RandomCompany"
+        val address = "546 st. random place"
+        val telephone = "+121231234567"
+        val telephone2 = "+121231234567"
+        val email = "example@example.com"
+        val alias = "MyCompany"
+
+        val company = Company(
+            rif,
+            name,
+            address,
+            telephone,
+            telephone2,
+            email,
+            alias
+        )
+
+        // when
+        companyManager?.addCompany(company)
+
+        // then
+        companyManager?.getCompanies()?.also {
+            val savedCompany = it.first()
+
+            assert(savedCompany.rif == company.rif)
+            assert(savedCompany.name == company.name)
+            assert(savedCompany.address == company.address)
+            assert(savedCompany.telephone == company.telephone)
+            assert(savedCompany.telephone2 == company.telephone2)
+            assert(savedCompany.email == company.email)
+            assert(savedCompany.alias == company.alias)
+        }
+    }
 }
