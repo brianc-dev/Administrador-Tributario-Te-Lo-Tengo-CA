0d50bbcd5321a0825874534148346ba8d37a9e9e
Brian Caldera <brian_alex07@outlook.com>
2023-04-14 02:23:28 -0400

    Fixed company table statement error.

--- src/test/kotlin/com/telotengoca/moth/model/CompanyManager.kt
+++ src/test/kotlin/com/telotengoca/moth/model/CompanyManager.kt
@@ -57,7 +57,7 @@ class CompanyManagerImpl(private val database: MothDatabase) : CompanyManager {
             logger.info("Creating table '{}'...", TABLE_NAME)
 
             con.createStatement().use {stm ->
-                stm.execute("CREATE TABLE IF NOT EXISTS `company`(`rif` VARCHAR(10)  PRIMARY KEY NOT NULL, `name` VARCHAR(50) NOT NULL, `address` VARCHAR(100), `telephone` VARCHAR(15), `telephone` VARCHAR(15), `email` VARCHAR(254), `alias` VARCHAR(50) NOT NULL)")
+                stm.execute("CREATE TABLE IF NOT EXISTS `company`(`rif` VARCHAR(10)  PRIMARY KEY NOT NULL, `name` VARCHAR(50) NOT NULL, `address` VARCHAR(100), `telephone` VARCHAR(15), `telephone_2` VARCHAR(15), `email` VARCHAR(254), `alias` VARCHAR(50) NOT NULL)")
             }
 
             logger.info("Table '{}' created", TABLE_NAME)
