10e4ba00bfa4a8ad742745eda872e737e064ca8e
Brian Caldera <brian_alex07@outlook.com>
2023-04-15 02:32:22 -0400

    Renamed CompanyView to CompanyViewTest

--- src/test/kotlin/com/telotengoca/moth/javafx/CompanyView.kt
+++ src/test/kotlin/com/telotengoca/moth/javafx/CompanyView.kt
@@ -6,18 +6,17 @@ import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.stage.Stage
 
-class CompanyView : Application() {
+class CompanyViewTest: Application() {
     override fun start(p0: Stage?) {
         val root: Parent = FXMLLoader.load(this::class.java.getResource("../view/companies.fxml"))
         val scene = Scene(root)
         p0?.let {stage ->
             stage.scene = scene
             stage.show()
-
         }
     }
 }
 
 fun main() {
-    Application.launch(CompanyView::class.java)
+    Application.launch(CompanyViewTest::class.java)
 }
