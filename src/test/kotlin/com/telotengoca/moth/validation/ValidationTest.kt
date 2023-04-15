39262bfe211baab6ef939f17432da31211134889
Brian Caldera <brian_alex07@outlook.com>
2023-04-15 13:09:32 -0400

    Added test dependency for Truth and some tests.

--- src/test/kotlin/com/telotengoca/moth/validation/ValidationTest.kt
+++ src/test/kotlin/com/telotengoca/moth/validation/ValidationTest.kt
@@ -1,25 +1,40 @@
 package com.telotengoca.moth.validation
 
+import com.google.common.truth.Truth
 import jakarta.validation.Validation
 import jakarta.validation.Validator
 import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotNull
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
 
 class ValidationTest {
+    
+    companion object {
 
-    data class User(
-        @get:Email(message = "Email must be a valid") val email: String?
-    )
+        private const val SIZE_MESSAGE = "The length must be between 2 and 50 characters"
+        private const val EMAIL_MESSAGE = "Email must be a valid"
+        private const val NOTNULL_MESSAGE = "Cannot be null"
+
+        private lateinit var validator: Validator
+        
+        @BeforeAll
+        @JvmStatic
+        fun `get validator`() {
+            val factory = Validation.buildDefaultValidatorFactory()
+            validator = factory.validator
+        }
+    }
 
     @Test
     fun `test that we can validate a email`() {
         // given
+        data class User(
+            @get:Email(message = EMAIL_MESSAGE) val email: String,
+        )
         val email = "username1example.com"
         val user = User(email)
-
-        val factory = Validation.buildDefaultValidatorFactory()
-        val validator: Validator = factory.validator
 
         // when
         val violations = validator.validate(user)
@@ -27,8 +42,49 @@ class ValidationTest {
         // then
         for (violation in violations) {
             print(violation.message)
+            Truth.assertThat(violation.message).matches(EMAIL_MESSAGE)
         }
 
         assert(violations.isNotEmpty())
+        assert(violations.size < 2)
+    }
+
+    @Test
+    fun `test that we can reject when name is min`() {
+        // given
+        data class User(
+            @get:jakarta.validation.constraints.Size(min = 2, max = 50, message = SIZE_MESSAGE) val name: String,
+        )
+        val name = "a"
+        val user = User(name)
+        // when
+        val violations = validator.validate(user)
+        // then
+        for (violation in violations) {
+            print(violation.message)
+            Truth.assertThat(violation.constraintDescriptor.annotation).isInstanceOf(jakarta.validation.constraints.Size::class.java)
+            Truth.assertThat(violation.message).matches(SIZE_MESSAGE)
+        }
+
+        assert(violations.isNotEmpty())
+        assert(violations.size < 2)
+    }
+
+    @Test
+    fun `test that we can reject null values`() {
+        // given
+        data class User(
+            @get:NotNull(message = NOTNULL_MESSAGE) val address: String?
+        )
+        val address = null
+        val user = User(address)
+        // when
+        val violations = validator.validate(user)
+        // then
+        Truth.assertThat(violations.isNotEmpty())
+        Truth.assertThat(violations.size).isEqualTo(1)
+        val violation = violations.first()
+        print(violation.message)
+        Truth.assertThat(violation.constraintDescriptor.annotation).isInstanceOf(NotNull::class.java)
     }
 }
